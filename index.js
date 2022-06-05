02a08d2fb6179b4db6a509a16c76bc9c3140e18e
DDiaz <daniel.diaz.0515@gmail.com>
2022-06-05 15:42:16 -0700

    practice button created

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const incrementBtn = document.getElementById("increment");
-console.log(incrementBtn);
+const countDisplay = document.getElementById("count-display");
+
+let currCount = 0;
+incrementBtn.addEventListener("click", () => {
+  currCount++;
+  countDisplay.innerText = currCount;
+});
