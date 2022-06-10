26a6ec6e7ea2db5bcc4e052457ba34233245cf49
DDiaz <daniel.diaz.0515@gmail.com>
2022-06-10 16:25:20 -0700

    search bar created, array of elements created

--- intro_to_react/src/components/SearchBar/SearchBar.js
+++ intro_to_react/src/components/SearchBar/SearchBar.js
@@ -1,16 +1,35 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
 
+const products = [
+  "tooth paste",
+  "tooth brush",
+  "mouthwash",
+  "dental floss",
+  "mouth guard",
+];
+
 const SearchBar = () => {
   const [searchValue, setsearchValue] = useState("");
 
   const handleInputChange = (event) => {
     setsearchValue(event.target.value);
   };
+
+  const handleClearClick = () => {
+    setsearchValue("");
+  };
+
+  const shouldDisplayButton = searchValue.length > 0;
+
   return (
     <div>
       <input type="text" value={searchValue} onChange={handleInputChange} />
-      {searchValue}
+      {shouldDisplayButton && <button onClick={handleClearClick}>clear</button>}
+
+      {products.map(() => {
+        return <li>{product}</li>;
+      })}
     </div>
   );
 };
